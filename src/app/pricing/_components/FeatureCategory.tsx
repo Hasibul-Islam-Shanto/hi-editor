d02d119f9d4f8cb1d2242585914018eb15499b4a
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2025-05-31 18:06:19 +0600

    feat: add payment integration
    Payment integration with lemon squeezy with webhooks

---
+++ src/app/pricing/_components/FeatureCategory.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FeatureCategory = ({
+  children,
+  label,
+}: {
+  children: React.ReactNode;
+  label: string;
+}) => {
+  return (
+    <div className="space-y-4">
+      <h3 className="text-sm font-medium tracking-wider text-gray-400 uppercase">
+        {label}
+      </h3>
+      <div className="space-y-3">{children}</div>
+    </div>
+  );
+};
+
+export default FeatureCategory;
